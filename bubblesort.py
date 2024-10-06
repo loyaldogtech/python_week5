fa55e3a108c10d657d300db24fda894244aebcd4
Shad Spreiter <loyaldogtech@gmail.com>
2024-10-06 15:58:51 -0700

    Bubble Sort

--- bubblesort.py
+++ bubblesort.py
@@ -1,10 +1,11 @@
 
-unsorted_list = [5, 3, 6, 2, 10]
+unsorted_list = [101, 49, 3, 12, 56]
 
 def bubblesort(the_list):
     high_idx = len(the_list) - 1
 
     for i in range(high_idx):
+        list_changed = False
         for j in range(high_idx):
             item = the_list[j]
             next = the_list[j + 1]
@@ -12,7 +13,11 @@ def bubblesort(the_list):
             if item > next:
                 the_list[j] = next
                 the_list[j + 1] = item
+                list_changed = True
 
             print(the_list, i, j)
+        print(list_changed)
+        if list_changed == False:
+            break
 
 bubblesort(unsorted_list)
