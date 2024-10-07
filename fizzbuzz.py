6afa32acde3e6f90c8b2478850cfeb838411cb29
Shad Spreiter <loyaldogtech@gmail.com>
2024-10-06 18:03:15 -0700

    FizzBuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+
+for i in range(1, 51):
+    fizzbuzz(i)
